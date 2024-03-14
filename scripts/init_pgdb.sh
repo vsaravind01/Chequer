3c5c2fcf01f6f32930b1846cef2599e08f95f15a
vsaravind01 <vsaravind01@gmail.com>
2024-03-14 19:53:31 +0530

    Update init_pgdb.sh

--- scripts/init_pgdb.sh
+++ scripts/init_pgdb.sh
@@ -1,4 +1,9 @@
-# create user ayush_admin_client
-sudo psql -U postgres -c "CREATE USER chequer_admin_client WITH PASSWORD 'chequer_admin_client_secret';"
-# create database ayush_connect with owner ayush_admin_client and encoding UTF8
-sudo psql -U postgres -c "CREATE DATABASE chequer_db WITH OWNER chequer_admin_client ENCODING 'UTF8';"
+DB_NAME=chequer_db
+DB_USER=chequer_admin_client
+DB_PASS=chequer_admin_client_secret
+
+# create user with password
+sudo -u postgres psql -c "CREATE USER $DB_USER WITH PASSWORD '$DB_PASS';"
+
+# create database with owner
+sudo -u postgres psql -c "CREATE DATABASE $DB_NAME WITH OWNER $DB_USER ENCODING 'UTF8';"
